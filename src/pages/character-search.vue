5ddb886a77794806230bfc14ef260e8e386d9768
violet4795 <violet4795@naver.com>
2021-10-13 18:31:02 +0900

    crawling error modify

--- src/pages/character-search.vue
+++ src/pages/character-search.vue
@@ -14,7 +14,41 @@ import axios from "axios";
   components: {}
 })
 export default class App extends Vue {
-  /* 
+  id: String = "";
+  async goCrawl() {
+    let characterData: any;
+
+    try {
+      characterData = await axios.get(`/lostark/Profile/Character/${this.id}`);
+      console.log(characterData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // async goCrawl() {
+  //   let characterData: any;
+
+  //   let apiServerResponse: any;
+  //   try {
+  //     characterData = await axios.get(`/lostark/Profile/Character/${this.id}`);
+
+  //     console.log(characterData);
+
+  //     characterData = await axios.get(`/lostark/Profile/Character/${this.id}`);
+
+  //     console.log(characterData);
+
+  //     apiServerResponse = await axios.get(`/api`);
+
+  //     console.log(apiServerResponse);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
+
+/* 
   read this doc 
   https://ichi.pro/ko/vue-js-baeg-endeu-seobeolo-peulogsihaneun-bangbeob-181843460187627
 
@@ -23,49 +57,36 @@ export default class App extends Vue {
 
 
    */
-  id: String = "";
-  async goCrawl() {
-    // console.log(
-    //   encodeURI("https://lostark.game.onstove.com/Profile/Character/슈수수")
-    // );
-    let characterData: any;
+//  주석
 
-    try {
-      // console.log(encodeURI(`/lostark`));
-      // console.log(
-      //   await axios.get(`/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98`)
-      // );
-      // const instance = axios.create();
-      // debugger;
-      // instance.defaults.headers.Host = "localhost:8080/lostark";
-      characterData = await axios.get(`/Profile/Character/${this.id}`);
-      // https://lostark.game.onstove.com/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98
-      //http://localhost:8080/lostark    /Profile/Character/%EC%8A%88%EC%88%98%EC%88%98
-      // characterData = await axios.get(
-      //   encodeURI(`/lostark/Profile/Character/${this.id}`)
-      //   // "/lostark/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
-      // );
+// console.log(encodeURI(`/lostark`));
+// console.log(
+//   await axios.get(`/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98`)
+// );
+// const instance = axios.create();
+// debugger;
+// instance.defaults.headers.Host = "localhost:8080/lostark";
 
-      // characterData = await axios.get(
-      //   encodeURI("https://www.naver.com/")
-      //   // "/lostark/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
-      // );
+// https://lostark.game.onstove.com/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98
+//http://localhost:8080/lostark    /Profile/Character/%EC%8A%88%EC%88%98%EC%88%98
+// characterData = await axios.get(
+//   encodeURI(`/lostark/Profile/Character/${this.id}`)
+//   // "/lostark/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
+// );
 
-      console.log(characterData);
+// characterData = await axios.get(
+//   encodeURI("https://www.naver.com/")
+//   // "/lostark/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
+// );
 
-      // characterData = await axios.get(
-      //   encodeURI(
-      //     `https://lostark.game.onstove.com/Profile/Character/${this.id}`
-      //   )
-      // );
-    } catch (error) {
-      console.log(error);
-    }
+// characterData = await axios.get(
+//   encodeURI(
+//     `https://lostark.game.onstove.com/Profile/Character/${this.id}`
+//   )
+// );
 
-    //   "https://lostark.game.onstove.com/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
-    // );
-  }
-}
+//   "https://lostark.game.onstove.com/Profile/Character/%EC%8A%88%EC%88%98%EC%88%98"
+// );
 </script>
 <style lang='scss'>
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,10 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
     proxy: {
-      "/loaa": {
+      "/lostark": {
         target: "https://lostark.game.onstove.com",
         pathRewrite: { "^/": "" },
         changeOrigin: true,
-        secure: false,
-        logLevel: "debug",
-      },
-      "/lostark": {
-        target: "https://lostark.game.onstove.com",
-        changeOrigin: false,
-        pathRewrite: { "^/": "" },
         secure: false,
         logLevel: "debug",
       },
